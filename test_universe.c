01af620f0fb552fa68f09ed28db217b8b8403879
Roy Smeding <roy.smeding@gmail.com>
2015-06-16 01:02:59 +0200

    Move create_universe to universe.c as universe_load

--- test_universe.c
+++ test_universe.c
@@ -4,63 +4,12 @@
 
 #include "universe.h"
 
-struct universe *create_universe(const char* filename) {
-	size_t num_planets;
-	struct universe *universe;
-	struct tm tm;
-
-	FILE *infile = fopen(filename, "r");
-	fscanf(infile, "%d %d %d %d %d %d",
-			&tm.tm_year, &tm.tm_mon, &tm.tm_mday,
-			&tm.tm_hour, &tm.tm_min, &tm.tm_sec
-		);
-
-	fscanf(infile, "%zu\n", &num_planets);
-
-	universe = universe_new(num_planets, mktime(&tm));
-
-	for(size_t planet = 0; planet < num_planets; planet++) {
-		int c;
-		char *name;
-		double mass, radius;
-		double x, y, z, vx, vy, vz;
-
-		// check comment
-		c = fgetc(infile);
-		while(c == '#') {
-			while(c != '\n')
-				c = fgetc(infile);
-			c = fgetc(infile);
-		}
-
-		ungetc(c, infile);
-
-		fscanf(infile, "%ms %lf %lf %lf %lf %lf %lf %lf %lf\n",
-				&name,
-				&mass, &radius,
-				&x, &y, &z,
-				&vx, &vy, &vz
-			);
-
-		planet_init(&universe->planets[planet],
-				name,
-				mass, radius,
-				x, y, z,
-				vx, vy, vz
-			);
-	}
-
-	fclose(infile);
-
-	return universe;
-}
-
 const char UNIVERSE_FILE[] = "universe.txt";
 
 int main(int argc, char **argv) {
 	// read data file
 	fprintf(stderr, "Loading universe file %s...\n", UNIVERSE_FILE);
-	struct universe *universe = create_universe(UNIVERSE_FILE);
+	struct universe *universe = universe_load(UNIVERSE_FILE);
 	fprintf(stderr, "Loaded %zd planets:\n", universe->num_planets);
 	fprintf(stderr, "\t%2s." " %-10s"
 			"\t%-12s"  "\t%-12s"
--- universe.c
+++ universe.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
@@ -27,4 +28,55 @@ void universe_free(struct universe *universe) {
 		free(universe->planets[planet].name);
 	free(universe->planets);
 	free(universe);
+}
+
+struct universe *universe_load(const char* filename) {
+	size_t num_planets;
+	struct universe *universe;
+	struct tm tm;
+
+	FILE *infile = fopen(filename, "r");
+	fscanf(infile, "%d %d %d %d %d %d",
+			&tm.tm_year, &tm.tm_mon, &tm.tm_mday,
+			&tm.tm_hour, &tm.tm_min, &tm.tm_sec
+		);
+
+	fscanf(infile, "%zu\n", &num_planets);
+
+	universe = universe_new(num_planets, mktime(&tm));
+
+	for(size_t planet = 0; planet < num_planets; planet++) {
+		int c;
+		char *name;
+		double mass, radius;
+		double x, y, z, vx, vy, vz;
+
+		// check comment
+		c = fgetc(infile);
+		while(c == '#') {
+			while(c != '\n')
+				c = fgetc(infile);
+			c = fgetc(infile);
+		}
+
+		ungetc(c, infile);
+
+		fscanf(infile, "%ms %lf %lf %lf %lf %lf %lf %lf %lf\n",
+				&name,
+				&mass, &radius,
+				&x, &y, &z,
+				&vx, &vy, &vz
+			);
+
+		planet_init(&universe->planets[planet],
+				name,
+				mass, radius,
+				x, y, z,
+				vx, vy, vz
+			);
+	}
+
+	fclose(infile);
+
+	return universe;
 }
